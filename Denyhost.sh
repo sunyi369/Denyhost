1ae8d251b9015d3820b6deeead3aaf3a4ef2aeeb
sunyi369 <25546385+sunyi369@users.noreply.github.com>
2024-01-24 10:53:51 +0800

    Update Denyhost.sh

--- Denyhost.sh
+++ Denyhost.sh
@@ -3,11 +3,9 @@ tar -zxvf DenyHosts-2.6.tar.gz
 cd DenyHosts-2.6
 python setup.py install
 cd /usr/share/denyhosts/
-cp daemon-control-dist daemon-control
-chown root daemon-control
-chmod 700 daemon-control
-ln -s /usr/share/denyhosts/daemon-control /etc/init.d
-/etc/init.d/daemon-control start
+cp /usr/local/share/denyhosts/daemon-control-dist /usr/local/bin/daemon-control
+chmod 700 /usr/local/bin/daemon-control
+daemon-control start
 chkconfig daemon-control on
 wget https://raw.githubusercontent.com/sunyi369/Denyhost/master/denyhosts.cfg /usr/share/denyhosts/denyhosts.cfg
 /etc/init.d/daemon-control restart
