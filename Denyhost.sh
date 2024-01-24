988dcc0a1c72ab4f3177226992adf83dcf2dd864
sunyi369 <25546385+sunyi369@users.noreply.github.com>
2024-01-24 11:20:23 +0800

    Update Denyhost.sh

--- Denyhost.sh
+++ Denyhost.sh
@@ -1,10 +1,5 @@
-yum install gcc make -y
-wget "https://www.python.org/ftp/python/2.7.5/Python-2.7.5.tgz"
-tar -zxvf Python-2.7.5.tgz
-cd Python-2.7.5
-./configure --prefix=/usr/local/python2.7.5 --enable-unicode=ucs4 --enable-shared
-make
-make install
+yum install python27
+cp /usr/bin/python2.7 /usr/bin/python
 wget "https://nchc.dl.sourceforge.net/project/denyhosts/denyhosts/2.6/DenyHosts-2.6.tar.gz"
 tar -zxvf DenyHosts-2.6.tar.gz
 cd DenyHosts-2.6
